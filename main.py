d93a3b48a3bb4e88ccdcbdd518ea954fc317a3c9
BAKI LEELA KOWSHIK REDDY <108167224+Kowshik001@users.noreply.github.com>
2024-05-13 21:22:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+
+# Use the official Python image as the base image
 FROM python:3.8
+
+# Set the working directory in the container
 WORKDIR /app
+
+# Copy the application files into the working directory
 COPY . /app
+
+# Install the application dependencies
 RUN pip install -r requirements.txt
-CMD ["python", "main.py"]
+
+# Define the entry point for the container
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
